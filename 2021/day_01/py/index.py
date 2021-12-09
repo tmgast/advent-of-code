94ac263a244ce2673bdbd99eec4645f709602e2c
= <tmgast@gmail.com>
2021-12-10 07:48:23 +0900

    solved python

--- 2021/day_01/py/index.py
+++ 2021/day_01/py/index.py
@@ -1,3 +1,6 @@
+"""
+    solving adjacent List item comparison
+"""
 import os
 
 with open(os.path.abspath(os.getcwd()+"/data.txt"),'r') as file:
@@ -13,3 +16,13 @@ for v in lines:
 
 print("solution 1: "+ str(counter))
 
+counter = 0
+calc = []
+for i in range(len(lines)):
+    calc.append(lines[i])
+    if len(calc) == 4:
+        if sum(calc[0:3]) < sum(calc[1:4]):
+            counter +=1
+        calc.pop(0)
+
+print("solution 2: "+ str(counter))
